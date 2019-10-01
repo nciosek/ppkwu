bead27407dffcd1bcfbde05eaa4542c01744a295
209286 <n_ciosek@wp.pl>
2019-10-01 17:02:45 +0200

    zadanie 1 - zmiana nazwy klasy

--- src/main/java/com/ppkwu/controller/ReverseController.java
+++ src/main/java/com/ppkwu/controller/ReverseController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SampleController {
+public class ReverseController {
 
     @GetMapping("/{str}")
     public String rev(@PathVariable String str){
