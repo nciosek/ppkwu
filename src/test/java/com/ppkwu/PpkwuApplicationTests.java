51ea9f54dff1ada74581a8cdbf7607eb1f1f0eea
209286 <n_ciosek@wp.pl>
2019-10-01 16:59:26 +0200

    zadanie 1 - stworzenie aplikacji

---
+++ src/test/java/com/ppkwu/PpkwuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ppkwu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpkwuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
