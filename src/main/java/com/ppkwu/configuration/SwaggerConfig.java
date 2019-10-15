75b412661ce166f0469d38f236df55f009d7c565
nciosek <n_ciosek@wp.pl>
2019-10-15 16:53:49 +0200

    zadanie 1 - dokumentacja

---
+++ src/main/java/com/ppkwu/configuration/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.ppkwu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket SwaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Reverse API")
+                .description(" ")
+                .contact(new Contact("Natalia Ciosek",
+                        "http://github.com/nciosek/ppkwu",
+                        "n_ciosek@wp.pl"))
+                .license("License name here")
+                .licenseUrl("URL to license")
+                .version("1.0.1")
+                .build();
+    }
+}
