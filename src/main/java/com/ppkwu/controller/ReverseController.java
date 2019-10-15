059bf20e058330597c57eb359e4ff2a7cac9a883
Natalia Ciosek <n_ciosek@wp.pl>
2019-10-15 17:04:32 +0200

    zadanie 2 - wystepowanie duzych,malych liter, liczb, znakow specjalnych, dowolnej kombinacji, zliczanie dlugosci przekazanego parametru

--- src/main/java/com/ppkwu/controller/ReverseController.java
+++ src/main/java/com/ppkwu/controller/ReverseController.java
@@ -2,13 +2,13 @@ package com.ppkwu.controller;
 
 import com.ppkwu.service.ReverseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReverseController {
-
     private ReverseService reverseService;
 
     @Autowired
@@ -16,14 +16,69 @@ public class ReverseController {
         this.reverseService = sampleService;
     }
 
+    @GetMapping("/validate/{data}")
+    public String validate(@PathVariable String data) {
+        return reverseService.validate(data);
+    }
+
+    @GetMapping("/length/{data}")
+    public ResponseEntity<Integer> getLengthOfText(@PathVariable String data) {
+        Integer lengthOfText = reverseService.getLengthOfText(data);
+        return ResponseEntity.ok(lengthOfText);
+    }
+
+    @GetMapping("/contain/whitespace/{data}")
+    public ResponseEntity<Boolean> hasAnyWhiteSpace(@PathVariable String data) {
+        Boolean hasAnyWhiteSpace = reverseService.hasAnyWhiteSpace(data);
+        return ResponseEntity.ok(hasAnyWhiteSpace);
+    }
+
+    @GetMapping("/contain/uppercase/{data}")
+    public ResponseEntity<Boolean> hasAnyUpperCaseLetter(@PathVariable String data) {
+        Boolean hasAnyUpperCase = reverseService.hasUpperCaseLetters(data);
+        return ResponseEntity.ok(hasAnyUpperCase);
+    }
+
+    @GetMapping("/contain/lowercase/{data}")
+    public ResponseEntity<Boolean> hasAnyLowerCaseLetter(@PathVariable String data) {
+        Boolean hasAnyLowerCase = reverseService.hasLowerCaseLetters(data);
+        return ResponseEntity.ok(hasAnyLowerCase);
+    }
+
+    @GetMapping("/contain/specialsymbol/{data}")
+    public ResponseEntity<Boolean> hasAnySpecialSymbol(@PathVariable String data) {
+        Boolean hasAnySpecialSymbol = reverseService.hasAnySpecialSymbols(data);
+        return ResponseEntity.ok(hasAnySpecialSymbol);
+    }
+
+    @GetMapping("/digit/{data}")
+    public ResponseEntity<Boolean> isDigit(@PathVariable String data) {
+        Boolean isDigit = reverseService.isDigit(data);
+        return ResponseEntity.ok(isDigit);
+    }
+
+    @GetMapping("/count/uppercase/{data}")
+    public ResponseEntity<Integer> getNumberOfUpperCaseLetters(@PathVariable String data) {
+        Integer numberOfUpperCaseLetters = reverseService.getNumberOfUpperCaseLetters(data);
+        return ResponseEntity.ok(numberOfUpperCaseLetters);
+    }
+
+    @GetMapping("/count/lowercase/{data}")
+    public ResponseEntity<Integer> getNumberOfLowerCaseLetters(@PathVariable String data) {
+        Integer numberOfLowerCaseLetters = reverseService.getNumberOfLowerCaseLetters(data);
+        return ResponseEntity.ok(numberOfLowerCaseLetters);
+    }
+
+    @GetMapping("/count/whitespaces/{data}")
+    public ResponseEntity<Integer> getNumberOfWhiteSpaces(@PathVariable String data) {
+        Integer numberOfWhiteSpaces = reverseService.getNumberOfWhiteSpaces(data);
+        return ResponseEntity.ok(numberOfWhiteSpaces);
+    }
+
     @GetMapping("/reverse/{data}")
     public String rev(@PathVariable String data) {
         return reverseService.reverse(data);
     }
 
-    @GetMapping("/validate/{data}")
-    public String validate(@PathVariable String data) {
-        return reverseService.validate(data);
-    }
 }
 
--- src/main/java/com/ppkwu/service/ReverseService.java
+++ src/main/java/com/ppkwu/service/ReverseService.java
@@ -5,10 +5,51 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReverseService {
 
-    public String reverse(String data) {
-        StringBuilder input = new StringBuilder();
-        input.append(data);
-        return input.reverse().toString();
+    public int getLengthOfText(String text) {
+        return text.length();
+    }
+
+    public boolean hasUpperCaseLetters(String text) {
+        return !text.equals(text.toLowerCase());
+    }
+
+    public boolean hasLowerCaseLetters(String text) {
+        return text.equals(text.toLowerCase());
+    }
+
+    public boolean hasAnySpecialSymbols(String text) {
+        return !text.chars().allMatch(Character::isLetter);
+    }
+
+    public boolean hasAnyWhiteSpace(String text) {
+        return text.contains(" ");
+    }
+
+    public boolean isDigit(String text) {
+        return text.matches("\\d+(\\.\\d+)?");
+    }
+
+    public int getNumberOfUpperCaseLetters(String text) {
+        return (int) text.codePoints()
+                .filter(c -> c >= 'A' && c <= 'Z')
+                .count();
+    }
+
+    public int getNumberOfLowerCaseLetters(String text) {
+        return (int) text.codePoints()
+                .filter(c -> c >= 'a' && c <= 'z')
+                .count();
+    }
+
+    public int getNumberOfWhiteSpaces(String text) {
+        int spaceCounter = 0;
+
+        for (char c : text.toCharArray()) {
+            if (c == ' ') {
+                spaceCounter++;
+            }
+        }
+        return spaceCounter;
     }
 
     public String validate(String data) {
@@ -27,4 +68,11 @@ public class ReverseService {
         }
         return result.toString();
     }
+
+    public String reverse(String data) {
+        StringBuilder input = new StringBuilder();
+        input.append(data);
+        return input.reverse().toString();
+    }
+
 }
